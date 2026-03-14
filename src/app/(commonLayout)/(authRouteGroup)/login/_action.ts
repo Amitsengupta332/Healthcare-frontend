ff0a81f442e41525671d3a1d3795cd2fcd22451e
amitsengupta <amitsengupta332@gmail.com>
2026-03-14 23:48:41 +0600

    Creating Server Action For Login

--- src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -1,8 +1,39 @@
-import { ILoginPayload } from "@/zod/auth.validation";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse } from "@/types/api.types";
+import { ILoginResponse } from "@/types/auth.types";
+import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
 
- 
+export const loginAction = async (payload : ILoginPayload ) : Promise<ILoginResponse | ApiErrorResponse> => {
+     const parsedPayload = loginZodSchema.safeParse(payload);
 
-export const loginAction = async (payload: ILoginPayload) => {
+         if(!parsedPayload.success){
+        const firstError = parsedPayload.error.issues[0].message || "Invalid input";
+        return {
+            success: false,
+            message: firstError,
+        }
+    }
   try {
-  } catch (error) {}
+    
+        const response = await httpClient.post<ILoginResponse>("/auth/login", parsedPayload.data);
+        console.log(response.data);
+
+          const { accessToken, refreshToken, token} = response.data;
+        // await setTokenInCookies("accessToken", accessToken);
+        // await setTokenInCookies("refreshToken", refreshToken);
+        // await setTokenInCookies("better-auth.session_token", token, 24 * 60 * 60); // 1 day in seconds
+
+        // redirect("/dashboard");
+  } catch (error:any) {
+     if(error && typeof error === "object" && "digest" in error && typeof error.digest === "string" && error.digest.startsWith("NEXT_REDIRECT")){
+        throw error;
+    }
+        return {
+            success: false,
+            message: `Login failed: ${error.message}`,
+        }
+    }
+  }
 };
